b78216a1316a4f2e2d13bb111e260e3842a1312e
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-08-01 20:24:34 +0330

    slider

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 /* menu */
-const menuToggle = document.getElementById('menu-toggle');
-const menuUl = document.querySelector('.menu-ul');
+const menuToggle = document.getElementById("menu-toggle");
+const menuUl = document.querySelector(".menu-ul");
 
-
-const closeBtn = document.createElement('div');
-closeBtn.classList.add('close-btn');
+const closeBtn = document.createElement("div");
+closeBtn.classList.add("close-btn");
 closeBtn.innerHTML = '<i class="fas fa-times"></i>';
 menuUl.prepend(closeBtn);
 
-
-menuToggle.addEventListener('click', () => {
-  menuUl.classList.add('active');
+menuToggle.addEventListener("click", () => {
+  menuUl.classList.add("active");
 });
 
-
-closeBtn.addEventListener('click', () => {
-  menuUl.classList.remove('active');
+closeBtn.addEventListener("click", () => {
+  menuUl.classList.remove("active");
+});
+/* slider */
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
