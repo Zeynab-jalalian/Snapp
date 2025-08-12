b75ef568db9fb3406d44fa7cce2106a4b6fcf677
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-08-12 18:11:51 +0330

    fix

--- script2.js
+++ script2.js
@@ -1,23 +1,30 @@
-
-const filterButtons = document.querySelectorAll(".category-of-join li ");
+const filterButtons = document.querySelectorAll(".category-of-join li");
 const jobBoxes = document.querySelectorAll(".join-box");
 
+function handleFilter(button) {
+ 
+  filterButtons.forEach((btn) => btn.classList.remove("active"));
+
+  button.classList.add("active");
+
+
+  const category = button.dataset.category;
+
+  jobBoxes.forEach((box) => {
+    if (category === "all" || box.dataset.category === category) {
+      box.style.display = "flex";
+    } else {
+      box.style.display = "none";
+    }
+  });
+}
+
+
 filterButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-   
-    filterButtons.forEach((btn) => btn.classList.remove("active"));
-
-    button.classList.add("active");
-
-    const category = button.dataset.category;
-
-
-    jobBoxes.forEach((box) => {
-      if (category === "all" || box.dataset.category === category) {
-        box.style.display = "flex"; 
-      } else {
-        box.style.display = "none";
-      }
+  ["click", "touchstart"].forEach((evt) => {
+    button.addEventListener(evt, (e) => {
+      e.preventDefault(); 
+      handleFilter(button);
     });
   });
 });
